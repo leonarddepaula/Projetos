f24e0f5884f77674af3c04472b69378b0546dc72
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2021-11-19 15:47:26 -0300

    Initial commit

---
+++ teste.py
@@ -0,0 +1,9 @@
+nome = str(input("Digite seu nome: "))
+
+peso = float(input("digite seu peso: "))
+
+altura = float(input("Digite sua altura: "))
+
+print("nome: ", nome)
+print("peso: ", peso)
+print("altura: ", altura)
