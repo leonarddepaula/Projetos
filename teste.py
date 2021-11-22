c835eeb710ec724219345a3d4adbd369d0e39f70
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2021-11-21 21:13:08 -0300

    atualizing

--- teste.py
+++ teste.py
@@ -4,6 +4,35 @@ peso = float(input("digite seu peso: "))
 
 altura = float(input("Digite sua altura: "))
 
-print("nome: ", nome)
-print("peso: ", peso)
-print("altura: ", altura)
+imc = peso / (altura*altura)
+
+
+
+if(imc > 18.5 and imc <24.9 ):
+    print("nome: ", nome)
+    print("peso: ", peso)
+    print("altura: ", altura)
+    print("Abaixo Do Peso Normal.")
+    
+elif( imc > 25.0 and imc < 29.9):
+    print("nome: ", nome)
+    print("peso: ", peso)
+    print("altura: ", altura)
+    print("Peso Normal.")
+
+elif(imc > 30.0 and imc <34.9):
+    print("nome: ", nome)
+    print("peso: ", peso)
+    print("altura: ", altura)
+    print("Obesidade Classe I.")
+
+elif(imc > 35.0  and imc < 39.9):
+    print("nome: ", nome)
+    print("peso: ", peso)
+    print("altura: ", altura)
+    priint("Obesidade Classe II.")
+else:
+    print("nome: ", nome)
+    print("peso: ", peso)
+    print("altura: ", altura)
+    print("Obesidader clase III.")
