7aaef6651c5b10f5bf5933b4ca3c4caf60e9257d
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2021-12-03 01:21:22 -0300

    ok
    ok

--- importPacotFunc.py
+++ importPacotFunc.py
@@ -1,1 +1,5 @@
 # mporttando pacotes e funções básicas.
+'''
+    pacotes são conjutos de modulos.
+    biblioteca são conjunto de pacotes
+'''
---
+++ instalPacotes.py
@@ -0,0 +1,13 @@
+# instalando pacotes no google colab.
+
+# 8.3 Pip
+# pip install pandas.
+# pip uninstall pandas.
+
+'''
+    importação de pacotes
+
+'''
+import pandas as pd
+import numpy as np
+pd.read_csv('')
