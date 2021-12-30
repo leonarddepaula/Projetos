08cc9562cca4927a12f7386b487b2dc4d5a6cabc
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2021-12-30 19:58:16 -0300

    update
    xml, html,javascript

--- Modulo3/mod3hello.py
+++ Modulo3/mod3hello.py
@@ -5,4 +5,9 @@ def lista_de_numeros():
     lista = pd.Series([12, 13, 145, 896, 5667])
     print(lista)
 
+# cmd
+# python --version
+# criando ambiente virtual : python -m venv ambiente
+# Ativando o ambiente: > soucer ambiente/scripts/activate
+
 lista_de_numeros()
